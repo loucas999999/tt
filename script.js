5a1b56dbb40e4fdfcfce2e7dfbf3b0cb619f22c5
Pardon <149908533+loucas999999@users.noreply.github.com>
2024-01-21 21:57:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function scrollToElement(elementSelector, instance = 0){
     const elements = document.querySelectorAll(elementSelector);
     if(elements.length > instance){
         elements[instance].scrollIntoView({behavior: 'smooth' });
-
     }
 }
 
